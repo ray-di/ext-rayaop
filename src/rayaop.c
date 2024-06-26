1aa1f3b9496baa09479623a66310102c65d52387
Akihito Koriyama <akihito.koriyama@gmail.com>
2024-06-27 00:54:23 +0900

    Update rayaop extension for function tracking
    Significant changes were made to the rayaop PHP extension. A new version of the zend_execute_ex function was added, which is called when the module initializes, replaces the original, and restores it upon shutting down the module. This update allows tracking of the current executing function name instead of the previous static message output.

--- src/rayaop.c
+++ src/rayaop.c
@@ -2,56 +2,85 @@
 #include "config.h"
 #endif
 
-// PHPの主要なヘッダーファイルをインクルード
 #include "php.h"
 #include "php_ini.h"
 #include "ext/standard/info.h"
-// エクステンション固有のヘッダーファイルをインクルード
-#include "include/php_rayaop.h"
+#include "zend_exceptions.h"
 
-// rayaop関数の実装
-PHP_FUNCTION(rayaop)
+// @link https://www.phpinternalsbook.com/php7/extensions_design/php_functions.html
+#include "zend_interfaces.h"
+
+// エクステンション固有のヘッダーファイルをインクルード
+#include "php_rayaop.h"
+
+// オリジナルのハンドラーを保存するための変数
+// @link https://www.phpinternalsbook.com/php7/internal_types/zend_function.html
+static void (*original_zend_execute_ex)(zend_execute_data *execute_data);
+
+// 新しい zend_execute_ex 関数
+static void rayaop_zend_execute_ex(zend_execute_data *execute_data)
 {
-    // 単純に "Hello AOP!" を出力
-    php_printf("Hello AOP!\n");
+    // 現在実行中の関数名を取得
+    // @link https://www.phpinternalsbook.com/php7/internal_types/zend_execute_data.html
+    zend_function *current_function = execute_data->func;
+
+    if (current_function->common.function_name) {
+        // 関数名が存在する場合、メッセージを出力
+        php_printf("Hello %s\n", ZSTR_VAL(current_function->common.function_name));
+    }
+
+    // オリジナルの zend_execute_ex を呼び出し
+    original_zend_execute_ex(execute_data);
 }
 
-// phpinfo()関数で表示される情報を設定
+// モジュール初期化時に呼ばれる関数
+// @link https://www.phpinternalsbook.com/php7/extensions_design/php_lifecycle.html
+PHP_MINIT_FUNCTION(rayaop)
+{
+    // オリジナルの zend_execute_ex を保存し、新しい関数に置き換え
+    original_zend_execute_ex = zend_execute_ex;
+    zend_execute_ex = rayaop_zend_execute_ex;
+
+    return SUCCESS;
+}
+
+// モジュール終了時に呼ばれる関数
+PHP_MSHUTDOWN_FUNCTION(rayaop)
+{
+    // オリジナルの zend_execute_ex を復元
+    zend_execute_ex = original_zend_execute_ex;
+
+    return SUCCESS;
+}
+
+// phpinfo() 関数で表示される情報を設定
 PHP_MINFO_FUNCTION(rayaop)
 {
-    // 情報テーブルの開始
     php_info_print_table_start();
-    // テーブルヘッダーを追加（2列）
     php_info_print_table_header(2, "rayaop support", "enabled");
-    // 情報テーブルの終了
     php_info_print_table_end();
 }
 
-// 関数の引数情報を指定（引数がないため空）
-ZEND_BEGIN_ARG_INFO(arginfo_rayaop, 0)
-ZEND_END_ARG_INFO()
-
 // エクステンションの関数エントリーを定義
 const zend_function_entry rayaop_functions[] = {
-    PHP_FE(rayaop, arginfo_rayaop)  // rayaop関数をPHPから呼び出し可能にする
     PHP_FE_END  // 関数エントリーの終了を示す
 };
 
 // エクステンションのエントリーポイントを定義
 zend_module_entry rayaop_module_entry = {
-    STANDARD_MODULE_HEADER,  // 標準のモジュールヘッダー
-    "rayaop",  // エクステンション名
-    rayaop_functions,  // エクステンションの関数リスト
-    NULL,  // PHPエクステンションの初期化関数（ここでは使用しない）
-    NULL,  // PHPエクステンションの終了関数（ここでは使用しない）
-    NULL,  // リクエスト開始時の関数（ここでは使用しない）
-    NULL,  // リクエスト終了時の関数（ここでは使用しない）
-    PHP_MINFO(rayaop),  // phpinfo()用の関数
-    "1.0",  // エクステンションのバージョン
-    STANDARD_MODULE_PROPERTIES  // 標準のモジュールプロパティ
+    STANDARD_MODULE_HEADER,
+    "rayaop",
+    rayaop_functions,
+    PHP_MINIT(rayaop),      // 初期化関数
+    PHP_MSHUTDOWN(rayaop),  // 終了関数
+    NULL,
+    NULL,
+    PHP_MINFO(rayaop),
+    PHP_RAYAOP_VERSION,
+    STANDARD_MODULE_PROPERTIES
 };
 
 // 動的にロード可能なモジュールとしてコンパイルされる場合
 #ifdef COMPILE_DL_RAYAOP
-ZEND_GET_MODULE(rayaop)  // エクステンションを取得するためのマクロ
+ZEND_GET_MODULE(rayaop)
 #endif
