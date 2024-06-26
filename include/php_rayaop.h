d546e1141bbf14002fb71149ecf9532d7635f240
Akihito Koriyama <akihito.koriyama@gmail.com>
2024-06-27 03:15:00 +0900

    Refactor module globals and clean up in php_rayaop.h
    The code has been refactored to streamline the module globals handling in the 'php_rayaop.h' header. The version definition was updated and unused code has been removed. Other adjustments include renaming the function and the removal of unnecessary comments.

--- include/php_rayaop.h
+++ include/php_rayaop.h
@@ -1,38 +1,25 @@
-#ifndef PHP_RAYAOP_H  // ヘッダーファイルが既にインクルードされていないか確認
-#define PHP_RAYAOP_H  // ヘッダーファイルをインクルードしたことをマーク
+#ifndef PHP_RAYAOP_H
+#define PHP_RAYAOP_H
 
-extern zend_module_entry rayaop_module_entry;  // エクステンションのエントリーポイントを宣言
-#define phpext_rayaop_ptr &rayaop_module_entry  // エクステンションのエントリーポイントへのポインタを定義
+extern zend_module_entry rayaop_module_entry;
+#define phpext_rayaop_ptr &rayaop_module_entry
 
-#define PHP_RAYAOP_VERSION "1.0"  // エクステンションのバージョンを定義
+#define PHP_RAYAOP_VERSION "1.0.0"  // バージョンを更新
 
-typedef struct _zend_rayaop_globals {
-    char *greeting;
-} zend_rayaop_globals;
-
-#ifdef PHP_WIN32  // Windows環境でコンパイルされる場合
-#   define PHP_RAYAOP_API __declspec(dllexport)  // DLLエクスポートを指定
-#elif defined(__GNUC__) && __GNUC__ >= 4  // GCC 4.0以上でコンパイルされる場合
-#   define PHP_RAYAOP_API __attribute__ ((visibility("default")))  // シンボルの可視性をデフォルトに設定
-#else  // それ以外の環境でコンパイルされる場合
-#   define PHP_RAYAOP_API  // 空のマクロを定義
+#ifdef ZTS
+#include "TSRM.h"
 #endif
 
-#ifdef ZTS  // スレッドセーフモードでコンパイルされる場合
-#include "TSRM.h"  // スレッドセーフリソースマネージャをインクルード
+ZEND_BEGIN_MODULE_GLOBALS(rayaop)
+    // グローバル変数をここに追加
+ZEND_END_MODULE_GLOBALS(rayaop)
+
+#define RAYAOP_G(v) ZEND_MODULE_GLOBALS_ACCESSOR(rayaop, v)
+
+#if defined(ZTS) && defined(COMPILE_DL_RAYAOP)
+ZEND_TSRMLS_CACHE_EXTERN()
 #endif
 
-PHP_FUNCTION(rayaop);  // rayaop関数を宣言
+PHP_FUNCTION(method_intercept);
 
-PHP_MINIT_FUNCTION(rayaop);  // エクステンションの初期化関数を宣言
-PHP_MSHUTDOWN_FUNCTION(rayaop);  // エクステンションの終了関数を宣言
-PHP_MINFO_FUNCTION(rayaop);  // phpinfo()用の関数を宣言
-
-#ifdef ZTS  // スレッドセーフモードでコンパイルされる場合
-#define RAYAOP_G(v) TSRMG(rayaop_globals_id, zend_rayaop_globals *, v)  // グローバル変数へのアクセスを定義
-#else  // スレッドセーフモードでない場合
-#define RAYAOP_G(v) (rayaop_globals.v)  // グローバル変数へのアクセスを定義
-#endif
-
-#endif  /* PHP_RAYAOP_H */  // ヘッダーファイルの終わり
-
+#endif /* PHP_RAYAOP_H */
