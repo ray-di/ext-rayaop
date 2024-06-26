33e816dce0376123b246ab2dab15f62c559038f9
Akihito Koriyama <akihito.koriyama@gmail.com>
2024-06-27 01:50:11 +0900

    Enhance debug features in rayaop.c
    The commit updates rayaop.c to improve its debugging capabilities. Debug print statements were added to provide more information during code execution. Also, an additional function, "rayaop_debug", was added for debugging purposes. Furthermore, message format and content for intercepting class methods have been revised for clarity.

--- src/rayaop.c
+++ src/rayaop.c
@@ -2,63 +2,77 @@
 #include "config.h"
 #endif
 
+// PHPの主要なヘッダーファイルをインクルード
 #include "php.h"
 #include "php_ini.h"
 #include "ext/standard/info.h"
 #include "zend_exceptions.h"
-
 // @link https://www.phpinternalsbook.com/php7/extensions_design/php_functions.html
 #include "zend_interfaces.h"
-
 // エクステンション固有のヘッダーファイルをインクルード
 #include "php_rayaop.h"
 
-// オリジナルのハンドラーを保存するための変数
-// @link https://www.phpinternalsbook.com/php7/internal_types/zend_function.html
+// オリジナルのzend_execute_ex関数へのポインタ
 static void (*original_zend_execute_ex)(zend_execute_data *execute_data);
 
-// 新しい zend_execute_ex 関数
+// 新しいzend_execute_ex関数の実装
 static void rayaop_zend_execute_ex(zend_execute_data *execute_data)
 {
-    // 現在実行中の関数名を取得
+    // 現在実行中の関数情報を取得
     // @link https://www.phpinternalsbook.com/php7/internal_types/zend_execute_data.html
     zend_function *current_function = execute_data->func;
 
-    // クラスメソッドの場合のみインターセプト
+    // デバッグ情報を出力
+    php_printf("Debug: Executing function\n");
+
+    // クラスメソッドの場合
     if (current_function->common.scope && current_function->common.function_name) {
-        // クラス名とメソッド名を取得
         const char *class_name = ZSTR_VAL(current_function->common.scope->name);
         const char *method_name = ZSTR_VAL(current_function->common.function_name);
 
-        // メッセージを出力
-        php_printf("Hello %s::%s\n", class_name, method_name);
+        // インターセプトメッセージを出力
+        php_printf("Intercepted: %s::%s\n", class_name, method_name);
+    }
+    // グローバル関数の場合
+    else if (current_function->common.function_name) {
+        php_printf("Debug: Global function: %s\n", ZSTR_VAL(current_function->common.function_name));
+    }
+    // その他の場合（匿名関数など）
+    else {
+        php_printf("Debug: Unknown function type\n");
     }
 
-    // オリジナルの zend_execute_ex を呼び出し
+    // オリジナルのzend_execute_ex関数を呼び出し
     original_zend_execute_ex(execute_data);
+}
+
+// デバッグ用の関数を追加
+PHP_FUNCTION(rayaop_debug)
+{
+    php_printf("Ray.Aop debug function called\n");
+    RETURN_TRUE;
 }
 
 // モジュール初期化時に呼ばれる関数
 // @link https://www.phpinternalsbook.com/php7/extensions_design/php_lifecycle.html
 PHP_MINIT_FUNCTION(rayaop)
 {
-    // オリジナルの zend_execute_ex を保存し、新しい関数に置き換え
+    // オリジナルのzend_execute_exを保存し、新しい関数に置き換え
     original_zend_execute_ex = zend_execute_ex;
     zend_execute_ex = rayaop_zend_execute_ex;
-
+    php_printf("Debug: Ray.Aop extension initialized\n");
     return SUCCESS;
 }
 
 // モジュール終了時に呼ばれる関数
 PHP_MSHUTDOWN_FUNCTION(rayaop)
 {
-    // オリジナルの zend_execute_ex を復元
+    // オリジナルのzend_execute_exを復元
     zend_execute_ex = original_zend_execute_ex;
-
     return SUCCESS;
 }
 
-// phpinfo() 関数で表示される情報を設定
+// phpinfo()関数で表示される情報を設定
 PHP_MINFO_FUNCTION(rayaop)
 {
     php_info_print_table_start();
@@ -68,6 +82,7 @@ PHP_MINFO_FUNCTION(rayaop)
 
 // エクステンションの関数エントリーを定義
 const zend_function_entry rayaop_functions[] = {
+    PHP_FE(rayaop_debug, NULL)  // デバッグ関数を追加
     PHP_FE_END  // 関数エントリーの終了を示す
 };
 
