ff19747f95100b54b57c53e7f7bc3ec823e941a9
Akihito Koriyama <akihito.koriyama@gmail.com>
2024-06-27 01:40:09 +0900

    Add global function and test class to rayaop.php
    This commit replaces the existing rayaop() and rayaop_advanced() functions with a global function (sayHello) and a test class (TestClass) in the rayaop.php file. The test class includes a static method call, an instantiation with a method call and a script execution completion message at the end.

--- rayaop.php
+++ rayaop.php
@@ -1,4 +1,33 @@
 <?php
 
-rayaop();
-rayaop_advanced();
+// グローバル関数（インターセプトされない）
+function sayHello($name) {
+    echo "sayHello($name) called\n";
+}
+
+// テスト用クラス
+class TestClass {
+    public function __construct() {
+        echo "TestClass constructed\n";
+    }
+
+    public function testMethod() {
+        echo "TestClass::testMethod() called\n";
+    }
+
+    public static function staticMethod() {
+        echo "TestClass::staticMethod() called\n";
+    }
+}
+
+// グローバル関数呼び出し（インターセプトされない）
+sayHello("World");
+
+// クラスのインスタンス化とメソッド呼び出し
+$test = new TestClass();
+$test->testMethod();
+
+// 静的メソッド呼び出し
+TestClass::staticMethod();
+
+echo "Script execution completed\n";
