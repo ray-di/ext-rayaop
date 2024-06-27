38401cbece0ee2130980b1024a7e239f9cb9c6ca
Akihito Koriyama <akihito.koriyama@gmail.com>
2024-06-27 15:43:37 +0900

    Refactor Intercepted and TestClass in rayaop.php
    The unnecessary namespace usage surrounding the Intercepted and TestClass objects in rayaop.php was removed, making the code cleaner and easier to read. In addition, the file now directly requires the InterceptedInterface.php script at the top.

--- rayaop.php
+++ rayaop.php
@@ -1,58 +1,52 @@
 <?php
 
-// namespace Ray\Aop {
-//     interface MethodInterceptorInterface
-//     {
-//         public function intercept(object $object, string $method, array $params): mixed;
-//     }
-// }
+require __DIR__ . '/php-src/InterceptedInterface.php';
 
-namespace {
-    class Intercepted implements Ray\Aop\MethodInterceptorInterface
+class Intercepted implements Ray\Aop\MethodInterceptorInterface
+{
+    public function intercept(object $object, string $method, array $params): mixed
     {
-        public function intercept(object $object, string $method, array $params): mixed
-        {
-            echo "Intercepted: " . get_class($object) . "::{$method}\n";
-            echo "Arguments: " . json_encode($params) . "\n";
-            
-            // 元のメソッドを呼び出し、結果を返す
-            return call_user_func_array([$object, $method], $params);
-        }
+        echo "Intercepted: " . get_class($object) . "::{$method}\n";
+        echo "Arguments: " . json_encode($params) . "\n";
+
+        // 元のメソッドを呼び出し、結果を返す
+        return call_user_func_array([$object, $method], $params);
     }
-
-    $intercepted = new Intercepted();
-    method_intercept('TestClass', 'testMethod', $intercepted);
-
-    class TestClass
-    {
-        public function __construct()
-        {
-            echo "TestClass constructed\n";
-        }
-
-        public function testMethod($arg)
-        {
-            echo "TestClass::testMethod($arg) called\n";
-            return "Result: $arg";
-        }
-
-        public function nonInterceptedMethod($arg)
-        {
-            echo "TestClass::nonInterceptedMethod($arg) called\n";
-            return "Non-intercepted result: $arg";
-        }
-    }
-
-    echo "Creating TestClass instance\n";
-    $test = new TestClass();
-    
-    echo "Calling testMethod (should be intercepted)\n";
-    $result1 = $test->testMethod("test");
-    echo "Result: $result1\n";
-    
-    echo "\nCalling nonInterceptedMethod (should not be intercepted)\n";
-    $result2 = $test->nonInterceptedMethod("test");
-    echo "Result: $result2\n";
-
-    echo "\nScript execution completed\n";
 }
+
+$intercepted = new Intercepted();
+method_intercept('TestClass', 'testMethod', $intercepted);
+
+class TestClass
+{
+    public function __construct()
+    {
+        echo "TestClass constructed\n";
+    }
+
+    public function testMethod($arg)
+    {
+        echo "TestClass::testMethod($arg) called\n";
+        return "Result: $arg";
+    }
+
+    public function nonInterceptedMethod($arg)
+    {
+        echo "TestClass::nonInterceptedMethod($arg) called\n";
+        return "Non-intercepted result: $arg";
+    }
+}
+
+echo "Creating TestClass instance\n";
+$test = new TestClass();
+
+echo "Calling testMethod (should be intercepted)\n";
+$result1 = $test->testMethod("test");
+echo "Result: $result1\n";
+
+echo "\nCalling nonInterceptedMethod (should not be intercepted)\n";
+$result2 = $test->nonInterceptedMethod("test");
+echo "Result: $result2\n";
+
+echo "\nScript execution completed\n";
+
